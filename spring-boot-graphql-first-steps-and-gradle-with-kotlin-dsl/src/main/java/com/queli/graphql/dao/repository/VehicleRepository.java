378b9d90530325101c4a4debc6d146775802d332
Queli Coto <quelicm@gmail.com>
2019-09-01 12:43:14 +0200

    feat: add graphql sample and gradle with kolin dsl

---
+++ spring-boot-graphql-first-steps-and-gradle-with-kotlin-dsl/src/main/java/com/queli/graphql/dao/repository/VehicleRepository.java
@@ -0,0 +1,9 @@
+package com.queli.graphql.dao.repository;
+
+import com.queli.graphql.dao.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+}
